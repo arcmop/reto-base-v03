dbcd939baa0516aef8bb3648682374f4f7c0b5aa
Your Name <you@example.com>
2019-11-30 10:06:32 -0500

    Add provision file

---
+++ provision.sh
@@ -0,0 +1,2 @@
+export MACHINE_STORAGE_PATH=D:\\DOCKERBIG
+docker-machine create docker-taller04 --driver virtualbox --virtualbox-cpu-count "1" --virtualbox-memory "4096" --virtualbox-disk-size "20480"
