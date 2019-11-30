20d5edc9dc81a8ac7a444fcac7c58ae6d5e2237d
Your Name <you@example.com>
2019-11-30 15:18:00 -0500

    Added Suma

---
+++ script-build.sh
@@ -0,0 +1,22 @@
+export PG_USER=pguser01
+export PG_PWD=pgpassword
+export PG_DB=pgdatabase
+
+eval $(docker-machine env docker-taller04)
+DOCKERMCHIP=$(docker-machine ip docker-taller04 || echo "127.0.0.1")
+RUTABASE=$PWD
+
+#Clean workspace
+rm -f docker-ms/*.jar
+
+#Build
+cd retobase-java
+docker run -v //$PWD:/opt -v //$RUTABASE/mvncache:/root/.m2 maven:3.6.2-jdk-8 sh -c "cd /opt && mvn -DskipTests clean install"
+cp target/*.jar ../docker-ms
+docker run -v //$PWD:/opt -v //$RUTABASE/mvncache:/root/.m2 maven:3.6.2-jdk-8 sh -c "cd /opt && mvn clean"
+cd ..
+
+#Delivery
+cd docker-ms
+docker build -t test01:v01 .
+cd ..
